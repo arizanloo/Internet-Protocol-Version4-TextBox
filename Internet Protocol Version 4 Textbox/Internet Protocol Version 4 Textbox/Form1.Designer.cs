515fbf178aa695d681218022f32bec1727deb0ed
Alireza <87603058+arizanloo@users.noreply.github.com>
2022-01-24 16:31:45 +0330

    Add files via upload

---
+++ Internet Protocol Version 4 Textbox/Internet Protocol Version 4 Textbox/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Internet_Protocol_Version_4_Textbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_ok);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "use the folowing IP address:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 35);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(55, 29);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 15);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(172, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ".";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(97, 30);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(37, 15);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(140, 30);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 15);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(183, 30);
+            this.textBox4.MaxLength = 3;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 15);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ok.Location = new System.Drawing.Point(97, 68);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(131, 36);
+            this.btn_ok.TabIndex = 10;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 144);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Internet Protocol Version 4 Textbox/Internet Protocol Version 4 Textbox/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Internet_Protocol_Version_4_Textbox
+{
+    public partial class Form1 : Form
+    {
+        int ip1, ip2, ip3, ip4; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            //do your stuff here...
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string textbox1_Str;
+            textbox1_Str = textBox1.Text;
+
+            if (textBox1.Text.Length == 3)//if length equals to 3 focus next textbox
+                textBox2.Focus();
+
+            if (textbox1_Str.Contains("."))//if dot is pressed, remove '.' and focus next textbox
+            {
+                textBox1.Text = textbox1_Str.Replace(".", "");
+                if (!(textBox1.Text == ""))//if the textbox is'nt null, focus next textbox
+                    textBox2.Focus();
+            }
+
+            if (textBox1.Text == "")
+                return;
+
+            ip1 = Convert.ToInt16(textBox1.Text);
+            if (ip1 > 223)//show an error and fix the incorrect ip entered
+            {
+                MessageBox.Show(ip1 + " is not a valid entry. please specify a value between 1 and 223.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Text = "223";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string textbox2_Str;
+            textbox2_Str = textBox2.Text;
+
+            if (textBox2.Text.Length == 3)//if length equals to 3 focus next textbox
+                textBox3.Focus();
+
+            if (textbox2_Str.Contains("."))//if dot is pressed, remove '.' and focus next textbox
+            {
+                textBox2.Text = textbox2_Str.Replace(".", "");
+                if (!(textBox2.Text == ""))//if the textbox is'nt null, focus next textbox
+                    textBox3.Focus();
+            }
+
+            if (textBox2.Text == "")
+                return;
+
+            ip2 = Convert.ToInt16(textBox2.Text);//show an error and fix the incorrect ip entered
+            if (ip2 > 225)
+            {
+                MessageBox.Show(ip2 + " is not a valid entry. please specify a value between 0 and 225.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text = "225";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string textbox3_Str;
+            textbox3_Str = textBox3.Text;
+
+            if (textBox3.Text.Length == 3)//if length equals to 3 focus next textbox
+                textBox4.Focus();
+
+            if (textbox3_Str.Contains("."))//if dot is pressed, remove '.' and focus next textbox
+            {
+                textBox3.Text = textbox3_Str.Replace(".", "");
+                if (!(textBox3.Text == ""))//if the textbox is'nt null, focus next textbox
+                    textBox4.Focus();
+            }
+            if (textBox3.Text == "")
+                return;
+
+            ip3 = Convert.ToInt16(textBox3.Text);
+            if (ip3 > 225)//show an error and fix the incorrect ip entered
+            {
+                MessageBox.Show(ip3 + " is not a valid entry. please specify a value between 0 and 225.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox3.Text = "225";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Contains("."))//if dot is pressed, remove '.' character. in this case no focus needed!
+                textBox4.Text = textBox4.Text.Replace(".", "");
+
+            if (textBox4.Text == "")
+                return;
+
+            ip4 = Convert.ToInt16(textBox4.Text);
+            if (ip4 > 225)//show an error and fix the incorrect ip entered
+            {
+                MessageBox.Show(ip4 + " is not a valid entry. please specify a value between 0 and 225.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox4.Text = "225";
+            }
+        }
+
+    
+    }
+}
